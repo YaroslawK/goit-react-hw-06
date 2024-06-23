495cb17ba7c4bc03cd90e6212ad997edb852f78e
Yaroslaw <xxjokerxx933@gmail.com>
2024-06-23 15:40:42 +0300

    delete props App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,46 +2,16 @@ import './App.css';
 import ContactForm from './components/ContactForm/ContactForm';
 import SearchBox from './components/SearchBox/SearchBox';
 import ContactList from './components/ContactList/ContactList';
-import { useSelector, useDispatch } from 'react-redux';
-import { selectContacts, addContact, deleteContact } from './redux/contactsSlice';
-import { selectNameFilter, changeFilter } from './redux/filtersSlice';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './redux/store';
-
 
 function App() {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectNameFilter);
-  const dispatch = useDispatch();
-
-  const handleAddContact = (newContact) => {
-    dispatch(addContact(newContact));
-  };
-
-  const handleDeleteContact = (id) => {
-    dispatch(deleteContact(id));
-  };
-
-  const handleFilterChange = (filterValue) => {
-    dispatch(changeFilter(filterValue));
-  };
-
-  const filteredContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
 
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
         <div className="container">
           <h1>Phonebook</h1>
-          <ContactForm addContact={handleAddContact} />
-          <SearchBox value={filter} onFilter={handleFilterChange} />
-          <ContactList contact={filteredContacts} deleteContact={handleDeleteContact} />
+          <ContactForm />
+          <SearchBox />
+          <ContactList />
         </div>
-      </PersistGate>
-    </Provider>
   );
 }
 
